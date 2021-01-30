0826e05e6264452b0c545098adecfccb75285672
soma1999 <so12ma131999@gmail.com>
2021-01-30 18:09:50 +0900

    Revert "Merge pull request #10 from soma1999/second_training"
    This reverts commit 177979ffcee71262d3dfae5c8e2472bd76534b9e, reversing
    changes made to bb95a0be566ec23dfaf051350bfe385d98181282.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -28,25 +28,14 @@ class CalendarsController < ApplicationController
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-    
+
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-       wday_num = Date.today.wday+x
-      
-       if wday_num >= 7
-         wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wdays => wdays[wday_num]}
-
-    
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
-     
     end
 
   end
